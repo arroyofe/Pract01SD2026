a2f7229dacd69a0febd33ccef99d486843885472
Fernando <118523392+arroyofe@users.noreply.github.com>
2026-02-22 10:07:28 +0100

    Primera carga de ficheros

---
+++ src/main/java/es/ubu/lsi/client/ChatClientImpl.java
@@ -0,0 +1,328 @@
+/**
+ * 
+ */
+package es.ubu.lsi.client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import es.ubu.lsi.common.ChatMessage;
+import es.ubu.lsi.common.ChatMessage.*;
+
+/**
+ * Implementa el chat del cliente 
+ * @author Fernando Arroyo
+ */
+public class ChatClientImpl implements ChatClient {
+	
+	//Puerto por defecto
+	private static int DEFAULT_PORT = 1500;
+	
+	//Puerto por defecto
+	private static String DEFAULT_HOST= "localhost";
+	
+	//Servidor que va a usar el cliente
+	private String server;
+	
+	//Código de usuario del cliente
+	private String username;
+	
+	//Puerto por el que se comunica el cliente
+	private int port;
+	
+	//
+	private boolean carryOn = true;
+	
+	//identificación numérica del cliente
+	private static int id;
+	
+	//Flujo de entrada
+	private ObjectInputStream in;
+	
+	//Flujo de salida
+	private ObjectOutputStream out;
+	
+	//Variable Listener
+	private ChatClientListener escuchaCliente;
+	
+	
+	//Variable socket para la gestión de las conexiones
+	private static Socket socket;
+	
+	//Hora 
+	private SimpleDateFormat hora = new SimpleDateFormat("HH:mm:ss");
+	
+	// Logger para seguimiento de errores
+	private static final Logger LOGGER = Logger.getLogger(ChatClientImpl.class.getName());
+
+	/**
+	 * Instantiates a new chat client impl.
+	 *
+	 * @param server the server
+	 * @param username the username
+	 * @param port the port
+	 */
+	/*
+	 * Constructor Crea los datos del chat con todos los datos por parámetro
+	 * 
+	 * @param server
+	 * @param username
+	 * @param port
+	 */
+	public ChatClientImpl(String server, String username, int port) {
+		this.server = server;
+		this.username = username;
+		this.port = port;
+		
+		try {
+			this.socket = new Socket(this.server,this.port);
+			out = new ObjectOutputStream(socket.getOutputStream());
+			
+		}catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "Erreur IO", e);
+			System.exit(1);;
+			
+		}
+	}
+	
+	/*
+	 * Constructor Crea los datos del chat con servidor y cliente.
+	 * El puerto se escoge con el valor por defecto
+	 * 
+	 * @param server
+	 * @param username
+	 */
+	public ChatClientImpl(String server, String username) {
+		this(server,username,DEFAULT_PORT);
+		
+	}
+	
+	/*
+	 * Constructor Crea los datos del chat con el cliente solamente.
+	 * 
+	 * El puerto y el servidor se escogen con el valor por defecto
+	 * 
+	 * @param username
+	 */
+	public ChatClientImpl(String username) {
+		this(username,DEFAULT_HOST,DEFAULT_PORT);
+		
+	}
+	/*
+	 * Metodo de inicio del chat por parte de un usuario
+	 * 
+	 * @return true si el arranque es correcto y false en caso contrario 
+	 */
+	@Override
+	public boolean start() {
+		try {
+			//Mensaje de bienvenida si la conexión es correcta
+			System.out.println("Fernando patrocina el mensaje: Son las: [" + hora.format(new Date() ) + 
+					"]. Conexión establecida correctamente");
+			
+			//Envío del nombre de usuario al servidor
+			ChatMessage mensaje = new ChatMessage(0,MessageType.MESSAGE,username);
+			
+			try {
+				in = new ObjectInputStream(socket.getInputStream());
+				sendMessage(mensaje);
+				mensaje = (ChatMessage) in.readObject();
+				
+				// Se actualiza el id con id del servidor
+				id=mensaje.getId();
+				System.out.println("Fernando patrocina el mensaje: Son las: [" + hora.format(new Date() ) + 
+						"]. El id recibido del servidor es: " + id);
+				
+				//Se lanza el hilo del cliente
+				new Thread(new ChatClientListener(in)).start();
+				
+			}catch(IOException | ClassNotFoundException e) {
+				e.getStackTrace();
+				System.exit(1);
+			}
+			
+			try (Scanner entrada = new Scanner(System.in)){
+				
+				while (carryOn) {
+					String texto = entrada.nextLine();
+					
+					switch(texto.toUpperCase()){
+					case "LOGOUT":
+						sendMessage(new ChatMessage(id,MessageType.LOGOUT,""));
+						carryOn=false;
+						return true;
+					
+					case "SHUTDOWN":
+						sendMessage(new ChatMessage(id,MessageType.SHUTDOWN,""));
+						carryOn = false;
+						return true;
+					
+					default:
+						sendMessage(new ChatMessage(id,MessageType.MESSAGE,texto));
+						break;
+					
+					}
+				}
+				
+			}
+			
+		}finally{
+			//Al terminar el chat se desconecta al usuario
+			disconnect();
+		}
+		
+		return true;
+	}
+	
+	/*
+	 * Gestión de mensajes del servidor relativos a cada usuario
+	 * 
+	 * @param mensaje a enviar
+	 * 
+	 */
+	@Override
+	public void sendMessage(ChatMessage message) {
+
+		try {
+			out.writeObject (message);
+		}catch (IOException e){
+			LOGGER.log(Level.SEVERE, "Erreur IO", e);
+			disconnect();
+		}
+
+	}
+	
+	/*
+	 * Desconexion del cliente
+	 */
+	@Override
+	public void disconnect() {
+		
+		// Se para el oyente
+		escuchaCliente.pararChat();
+		
+		// Se pasa el carriOn a false para indicar el final
+		carryOn = false;
+		
+				
+		//Se cierran los streams y el socket
+		try {
+			out.close();
+			in.close();
+			socket.close();
+			
+		}catch(IOException e) {
+			LOGGER.log(Level.SEVERE, "Erreur IO", e);
+		}
+		
+		System.exit(0);
+
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+		// Variables para gestión de los argumentos
+		String server = null; // identificación del servidor que se recibe por parámetro
+		String username = null; // identificación del usuario que se recibe por parámetro
+		int port =0;
+		// En función de los parámetros recibidos se actualiza el valor de las variables
+		switch (args.length) {
+		case 1: // Cuando hay solamente un argumento, éste corresponde al usuario
+			server = DEFAULT_HOST; // El servidor será el servidor por defecto
+			username = args[0]; // El usuario será el que se pase por argumento
+			port=DEFAULT_PORT; // El puerto sera el puerto por defecto
+			break;
+
+		case 2: // en este caso se reciben ambos datos
+			server = args[0]; // el servidor en la primera posición
+			username = args[1]; // el usuario en la segunda
+			port=DEFAULT_PORT; // El puerto sera el puerto por defecto
+			break;
+			
+		case 3: // en este caso se reciben ambos datos
+			server = args[0]; // el servidor en la primera posición
+			username = args[1]; // el usuario en la segunda
+			port=Integer.parseInt(args[0]); // El puerto en la tecera
+			break;
+
+		default: // En cualquier otro caso se envia mensaje de advertencia
+			System.out.println("Fernando patrocina el mensaje:"
+					+ "Error. Pasar por parámetos [servidor] (opcional) <usuario> (obligatorio) [puerto] (opcional)");
+			break;
+		}
+
+		System.out.println("Fernando patrocina el mensaje: Bienvenido: " + username);
+		System.out.println("Fernando patrocina el mensaje: Conectando al puerto: " + port +
+				" del servidor: " + server);
+
+		// Una vez registrados los parámetros se lanza el chat
+		ChatClientImpl cliente = new ChatClientImpl(server, username,port);
+		cliente.start();
+		
+
+	}
+	
+	class ChatClientListener implements Runnable{
+		//Variable de Entrada de datos
+		private ObjectInputStream in;
+		
+		// Booleano para saber si el cliente está activo y actuar en consecuencia
+		private boolean activo=true;
+		
+		/*
+		 * Arranca el oyente
+		 * 
+		 * @param in
+		 * @param active
+		 */
+		public ChatClientListener(ObjectInputStream in) {
+			this.in = in;
+			activo = true;
+		}
+
+		/*
+		 * Para el chat del cliente 
+		 */
+		public void pararChat() {
+			activo= false;
+			carryOn = false;
+		}
+		
+		
+		/*
+		 * Arranca el chat
+		 */
+		@Override
+		public void run() {
+			
+				// Se reciben los mensajes en  bucle
+				while (carryOn) {
+					ChatMessage mensaje;
+					try {
+						mensaje = (ChatMessage) in.readObject();
+						System.out.println(mensaje.getMessage());
+						
+					} catch (ClassNotFoundException | IOException e) {
+						
+						LOGGER.log(Level.SEVERE, "Erreur IO", e);
+					}
+				}
+		}
+	}
+		
+
+}
---
+++ src/main/java/es/ubu/lsi/server/ChatServerImpl.java
@@ -0,0 +1,414 @@
+/**
+ * 
+ */
+package es.ubu.lsi.server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import es.ubu.lsi.common.ChatMessage;
+import es.ubu.lsi.common.ChatMessage.MessageType;
+
+/**
+ * Implementación del servidor del chat
+ * 
+ */
+public class ChatServerImpl implements ChatServer {
+	
+	// Puerto por defecto
+	private static int DEFAULT_PORT =1500;
+	
+	//Identificación del cliente
+	private static int  clientId=0;
+	
+	//Hora 
+	private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+	
+	//Puerto que se usa
+	private int port;
+	
+	//Booleano que indica si la sesión está activa
+	private boolean alive;
+	
+	//Socket del usuario
+	private Socket socket;
+	
+	//Socket del servidor
+	ServerSocket socketServ ;
+	
+	//Diccionario con los datos de los usuarios
+	Map<String,ServerThreadForClient> cjtoHilosClientes;
+	
+	//Diccionario con los datos de los usuarios
+	Map<Integer,String> cjtoClientes;
+	
+	//Diccionario de clientes baneados
+	Map<String,Boolean> cjtoClientesBaneados;
+	
+	//Logger para la gestión de errores
+	private static final Logger LOGGER = Logger.getLogger(ChatServerImpl.class.getName());
+
+
+	/**
+	 * Constructor
+	 * 
+	 * Constructor con el puerto 1500 por defecto
+	 */
+	public ChatServerImpl() {
+		this(DEFAULT_PORT);
+		this.alive=true;
+		this.cjtoClientes = new HashMap<Integer,String>();
+		this. cjtoClientesBaneados = new HashMap<String,Boolean>();
+		this.cjtoHilosClientes = new HashMap<String,ServerThreadForClient>() ;
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * Constructor con el puerto como argumento introducido por parámetro
+	 * 
+	 * @param port
+	 * @param alive
+	 */
+	public ChatServerImpl(int port) {
+		this.port=port;
+		this.alive=true;
+		this.cjtoClientes = new HashMap<Integer,String>();
+		this. cjtoClientesBaneados = new HashMap<String,Boolean>();
+		this.cjtoHilosClientes = new HashMap<String,ServerThreadForClient>() ;
+	}
+	
+	private synchronized int getNextId() {
+		return ++clientId;
+	}
+	
+	@Override
+	public void startup() {
+	
+		// Determinación del puerto
+	//	this.port = DEFAULT_PORT;
+		
+		try {
+			//Creación del socket para el server
+			this.socketServ = new ServerSocket(this.port);
+			
+			//Comunicación de apertura del socket
+			System.out.println("Fernando patrocina el mensaje: Servidor iniciado a las "
+					+ sdf.format(new Date()));
+			System.out.println("Escuchando al puerto: " + port );
+			
+			//Bucle de gestión de los mensajes de los clientes
+			while (alive) {
+				// El socket abierto para el servidor se usa para el cliente
+				socket = socketServ.accept();
+				
+				// Inicio del hilo del cliente
+				ServerThreadForClient hiloCliente = new ServerThreadForClient(socket);
+				
+				//Se arranca el hilo recién creado
+				hiloCliente.start();
+			}
+			
+		}catch(IOException e) {
+			e.getStackTrace();
+		}
+
+	}
+	
+	/*
+	 * para el sistema 
+	 */
+	@Override
+	public void shutdown() {
+		// Se marca como no activo al servidor
+		alive = false;
+		
+		// Parada de los chats de los usuarios
+		for (ServerThreadForClient chat :cjtoHilosClientes.values()) {
+			// Se informa del cierre
+			System.out.println("Se va a cerrar el chat.");
+			chat.stopChat();
+		}
+		
+		// Vaciado de la lista de clientes
+		cjtoClientes.clear();
+		
+		//Una vez parados los chats, se cierra el resto
+		try {
+			
+			// Se informa del cierre
+			System.out.println("Se va a apagar el servidor.");
+			
+			//Cierre del socket
+			socket.close();
+			socketServ.close();
+			
+		}catch(IOException e) {
+			LOGGER.log(Level.SEVERE, "Erreur IO", e);
+		}
+		
+		//Una vez cerrado todo se sale del sistema
+		System.exit(0);
+
+	}
+	
+	/*
+	 *  Envia los mensajes recibidos a todos los clientes presentes en el chat
+	 *  
+	 *  @param mensaje
+	 */
+	@Override
+	public void broadcast(ChatMessage mensaje) {
+
+		int usuarioEnvio = mensaje.getId();
+		MessageType tipo = mensaje.getType();
+		String contenido = mensaje.getMessage();
+
+		// Antes de enviar, se comprueba si es mensaje de baneo/desbaneo
+		String[] analisis = contenido.trim().split("\\s+", 2);
+
+		if (analisis.length > 1) {
+			String orden = analisis[0];
+			String usuarioBan = analisis[1];
+			if (orden.equals("ban")&& !cjtoClientesBaneados.containsKey(usuarioBan)) {
+				cjtoClientesBaneados.put(usuarioBan, true);
+				// Solamente para chequear el conjunto de baneados
+				cjtoClientesBaneados.forEach((key, value) -> System.out.println("Cliente baneado clave: " + key + " nick de usuario: " + value));
+
+			} else if (orden.equals("unban") && cjtoClientesBaneados.containsKey(usuarioBan)) {
+				cjtoClientesBaneados.remove(usuarioBan, true);
+				// Solamente para chequearel conjunto de baneados
+				cjtoClientesBaneados.forEach((key, value) -> System.out.println("Cliente baneado clave: " + key + " nick de usuario: " + value));
+			}
+		}
+		// Envío de los mensajes a todos los clientes no baneados
+		for (ServerThreadForClient cliente : cjtoHilosClientes.values()) {
+			String usuario = "";
+			for(Entry<String,ServerThreadForClient> entrada : cjtoHilosClientes.entrySet()) {
+				if (entrada.getValue() == cliente) {
+					usuario = entrada.getKey();
+				}
+			}
+			// Si el usuario está baneado no se envía el mensaje
+			if (cjtoClientesBaneados.get(usuario) != null) {
+				return;
+			} else {
+
+				ChatMessage nuevo = new ChatMessage(usuarioEnvio, tipo, contenido);
+				cliente.sendMessage(nuevo);
+
+			}
+		}
+
+	}
+	
+	/*
+	 * Elimina un cliente de la lista usando su id
+	 * 
+	 * @param id
+	 */
+	@Override
+	public void remove(int id) {
+		// Se informa en primer lugar al usuario que se va a cerrar 
+		broadcast(new ChatMessage(id,MessageType.MESSAGE,"Fernando patrocina el mensaje: se va a eliminar el usuario " + id));
+		
+		// Se recupera el usuario para cerrarel hilo corresponciente y 
+		// se para el chat del usuario
+		cjtoHilosClientes.get(cjtoClientes.get(id)).stopChat();
+		
+		// Se elimina el hilo
+		cjtoHilosClientes.remove(cjtoClientes.get(id));
+		
+		//Se elimina el cliente del diccionario
+		cjtoClientes.remove(id);
+		
+		// Si el cliente está en la lista de baneados se elimina igualmente
+		if(cjtoClientesBaneados.get(cjtoClientes.get(id)) != null) {
+			cjtoClientesBaneados.remove(cjtoClientes.get(id));
+		}
+
+	}
+	
+		
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Se lanza el servidor
+		ChatServerImpl chatServidor = new ChatServerImpl(DEFAULT_PORT);
+		chatServidor.startup();
+
+	}
+	
+	class ServerThreadForClient extends Thread{
+		
+		//Identificación del usuario
+		private int id;
+		
+		//Nombre del usuario
+		private String username;
+		
+		// Socket que usa el servidor
+		private Socket socket;
+		
+		//Flujo de entrada
+		private ObjectInputStream in;
+		
+		//Flujo de salida
+		private ObjectOutputStream out;
+		
+		private boolean activo = true;
+		
+
+		/*
+		 * Constructor
+		 * @param id
+		 * @param socket
+		 */
+		public ServerThreadForClient( Socket socket) {
+		//	this.id = id;
+		//	this.username = username; 
+			this.socket = socket;
+			this.activo = true;
+			
+			//Creación de los streams I/O
+			try {
+				this.in = new ObjectInputStream(socket.getInputStream());
+				this.out = new ObjectOutputStream(socket.getOutputStream());
+				
+			}catch(IOException e) {
+				e.printStackTrace();
+				try {// Si falla se cierra el socket
+					socket.close();
+				} catch (IOException e1) {
+					LOGGER.log(Level.SEVERE, "Erreur IO", e);
+				}
+			}
+		}
+		
+		public void sendMessage(ChatMessage mensaje) {
+			// Envio de un mensaje
+			try {
+				out.writeObject(mensaje);
+				
+			}catch(IOException e) {
+				LOGGER.log(Level.SEVERE, "Erreur IO", e);
+			}
+			
+		}
+
+		/*
+		 * Para un chat
+		 */
+		public void stopChat() {
+			//Para parar el chat se pone el booleano a false
+			activo=false;
+			
+		}
+		
+		
+
+		/*
+		 * run tramita los mensajes que vayan llegando
+		 * 
+		 * espera los mensajes de los clientes y realiza las operaciones correspondientes
+		 */
+		public void run() {
+			
+			try {
+			
+				// Lectura del primer mensaje con el nombre de usuario
+				ChatMessage primerMensaje = (ChatMessage) in.readObject();
+				
+				this.username = primerMensaje.getMessage();
+				
+				String usuario = primerMensaje.getMessage();
+				
+				if (cjtoClientes.containsValue(usuario)) {
+					out.writeObject(new ChatMessage(0,MessageType.LOGOUT,"Ese usuario ya está registrado"));
+				}else {
+					// Se añade la información del nuevo usuario y del hilo
+					this.id = getNextId();
+					cjtoClientes.put(id,usuario);
+					cjtoHilosClientes.put(usuario, this);
+					
+				}
+				
+							
+				//Se informa de la conexión del usuario
+				System.out.println("Fernando patrocina el mensaje : El usuario " + usuario + " con ip " 
+						+ this.socket.getInetAddress().getHostName() +" y con id:" 
+						+ this.id +" se ha conectado a las: " + ChatServerImpl.sdf.format(new Date( ))  );
+				
+				System.out.println("Clientes conectados: "+ cjtoHilosClientes.size());
+				
+				String bienvenida = String.format("Hola " + username + "Tu id es: " + id +
+						"Puedes empezar a chatear");
+				
+				out.writeObject(new ChatMessage(id,MessageType.MESSAGE,bienvenida));
+				
+				
+				//Bucle degestión de los mensajes
+				while(activo) {
+					//Se lee el mensaje del cliente
+					ChatMessage mensaje = (ChatMessage) in.readObject();
+					
+					//Si el cliente envía el mensaje de logout se le desactiv
+					if(mensaje.getType()==MessageType.LOGOUT) {
+						
+						System.out.println("Fernando patrocina el mensaje : " + this.username +" con id:" 
+								+ this.id + " se ha desconectado a las: " + ChatServerImpl.sdf.format(new Date( )));						
+						activo = false;
+						remove(id);
+						
+					}else if(mensaje.getType()==MessageType.SHUTDOWN) {
+						activo = false;//SHUTDOWN no se requiere en la práctica
+						System.out.println("Fernando patrocina el mensaje : " + this.username +" con id:" 
+								+ this.id + " se ha cerrado el servidor a las: " + ChatServerImpl.sdf.format(new Date( )));	
+						System.exit(1);
+					}else { //En caso contrario se publica el mensaje 
+						System.out.println("Fernando patrocina el mensaje : El usuario " + this.username +" con id:" 
+								+ this.id + " ha publicado a las: " + ChatServerImpl.sdf.format(new Date( )));
+						
+						broadcast(mensaje);
+						
+					}
+					
+				}
+				
+			}catch(IOException e) {
+				
+				LOGGER.log(Level.SEVERE, "Erreur IO", e);
+				
+			} catch (ClassNotFoundException e) {
+				
+				LOGGER.log(Level.SEVERE, "Erreur IO", e);
+				
+			}finally {// Se cierra el chat y sus componentes cuanto finaliza el cliente
+				try {
+					in.close();
+					out.close();
+					socket.close();
+				//	chatServer.remove(id);
+				} catch (IOException e) {
+					
+					LOGGER.log(Level.SEVERE, "Erreur IO", e);
+				}		
+			}
+			
+		}
+		
+	}
+
+}
